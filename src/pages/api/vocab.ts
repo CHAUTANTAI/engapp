36106b65b8cdadc092f1e00fbc3a5b15378f024f
ChauTanTai <chautantai201102@gmail.com>
2025-01-05 01:24:24 +0700

    fix eslint

--- src/pages/api/vocab.ts
+++ src/pages/api/vocab.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextApiRequest, NextApiResponse } from "next";
 import connection from "../../lib/db";
 
